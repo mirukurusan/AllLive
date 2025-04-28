553aa0254bdeee01783798a78502c4dfb638165c
Bolddriver <86687241+Bolddriver@users.noreply.github.com>
2025-04-28 09:49:45 +0800

    修复抖音实时在线人数显示异常

--- AllLive.Core/Danmaku/DouyinDanmaku.cs
+++ AllLive.Core/Danmaku/DouyinDanmaku.cs
@@ -181,7 +181,7 @@ namespace AllLive.Core.Danmaku
                 NewMessage?.Invoke(this, new LiveMessage()
                 {
                     Type = LiveMessageType.Online,
-                    Data = roomUserSeqMessage.totalUser,
+                    Data = roomUserSeqMessage.Total,
                     Color = DanmakuColor.White,
                     Message = "",
                     UserName = "",
