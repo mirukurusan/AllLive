f9bf037c82c9f126c95fc10d062a8719515ad6b7
mirukurusan <86687241+mirukurusan@users.noreply.github.com>
2026-08-10 11:15:06 +0800

    优化网格宽度

--- AllLive.WinUI/Controls/MyAdaptiveGridView.cs
+++ AllLive.WinUI/Controls/MyAdaptiveGridView.cs
@@ -1,19 +1,76 @@
 using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+using Windows.Foundation.Collections;
 
 namespace AllLive.WinUI.Controls
 {
-    // Rewritten: GridView removed in CommunityToolkit 8.x, using GridView as base
+    // 自适应网格：由 CommunityToolkit.Uwp.UI.Controls.AdaptiveGridView (v7.1) 移植而来，
+    // 用于在 CommunityToolkit 8.x 移除 AdaptiveGridView 之后恢复自适应尺寸计算。
     public class MyAdaptiveGridView : GridView
     {
+        private bool _needContainerMarginForLayout;
+
+        private ScrollViewer scrollViewer;
+
+        public MyAdaptiveGridView()
+        {
+            IsTabStop = false;
+            UseLayoutRounding = false;
+            SizeChanged += OnGridSizeChanged;
+            Items.VectorChanged += ItemsOnVectorChanged;
+        }
+
+        // ============ 自适应尺寸属性 ============
+
+        private static readonly DependencyProperty ItemWidthProperty =
+            DependencyProperty.Register("ItemWidth", typeof(double), typeof(MyAdaptiveGridView), new PropertyMetadata(double.NaN));
+
+        public static readonly DependencyProperty DesiredWidthProperty =
+            DependencyProperty.Register("DesiredWidth", typeof(double), typeof(MyAdaptiveGridView), new PropertyMetadata(double.NaN, OnDesiredWidthChanged));
+
+        public static readonly DependencyProperty ItemHeightProperty =
+            DependencyProperty.Register("ItemHeight", typeof(double), typeof(MyAdaptiveGridView), new PropertyMetadata(double.NaN));
+
+        public static readonly DependencyProperty OneRowModeEnabledProperty =
+            DependencyProperty.Register("OneRowModeEnabled", typeof(bool), typeof(MyAdaptiveGridView), new PropertyMetadata(false));
+
+        public static readonly DependencyProperty StretchContentForSingleRowProperty =
+            DependencyProperty.Register("StretchContentForSingleRow", typeof(bool), typeof(MyAdaptiveGridView), new PropertyMetadata(true, OnStretchContentForSingleRowChanged));
+
+        public double DesiredWidth
+        {
+            get { return (double)GetValue(DesiredWidthProperty); }
+            set { SetValue(DesiredWidthProperty, value); }
+        }
+
+        public double ItemHeight
+        {
+            get { return (double)GetValue(ItemHeightProperty); }
+            set { SetValue(ItemHeightProperty, value); }
+        }
+
+        public bool OneRowModeEnabled
+        {
+            get { return (bool)GetValue(OneRowModeEnabledProperty); }
+            set { SetValue(OneRowModeEnabledProperty, value); }
+        }
+
+        public bool StretchContentForSingleRow
+        {
+            get { return (bool)GetValue(StretchContentForSingleRowProperty); }
+            set { SetValue(StretchContentForSingleRowProperty, value); }
+        }
+
+        private double ItemWidth
+        {
+            get { return (double)GetValue(ItemWidthProperty); }
+            set { SetValue(ItemWidthProperty, value); }
+        }
+
+        // ============ 加载更多（保留原有逻辑） ============
 
         private ICommand _LoadMoreCommand;
         public ICommand LoadMoreCommand
@@ -21,6 +78,7 @@ namespace AllLive.WinUI.Controls
             get { return _LoadMoreCommand; }
             set { _LoadMoreCommand = value; }
         }
+
         public bool CanLoadMore { get; set; } = false;
 
         public double LoadMoreBottomOffset
@@ -29,14 +87,8 @@ namespace AllLive.WinUI.Controls
             set { SetValue(LoadMoreBottomOffsetProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for LoadMoreBottomOffset.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty LoadMoreBottomOffsetProperty =
             DependencyProperty.Register("LoadMoreBottomOffset", typeof(double), typeof(MyAdaptiveGridView), new PropertyMetadata(100));
-
-
-
-
-
 
         public bool DataLoading
         {
@@ -44,82 +96,171 @@ namespace AllLive.WinUI.Controls
             set { SetValue(DataLoadingProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for Loading.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty DataLoadingProperty =
             DependencyProperty.Register("DataLoading", typeof(bool), typeof(MyAdaptiveGridView), new PropertyMetadata(true));
 
+        // ============ 生命周期 ============
 
-
-
-
-        // Stub properties from AdaptiveGridView (removed in CommunityToolkit 8.x)
-        public double DesiredWidth
-        {
-            get { return (double)GetValue(DesiredWidthProperty); }
-            set { SetValue(DesiredWidthProperty, value); }
-        }
-        public static readonly DependencyProperty DesiredWidthProperty =
-            DependencyProperty.Register("DesiredWidth", typeof(double), typeof(MyAdaptiveGridView), new PropertyMetadata(250.0));
-
-        public double ItemHeight
-        {
-            get { return (double)GetValue(ItemHeightProperty); }
-            set { SetValue(ItemHeightProperty, value); }
-        }
-        public static readonly DependencyProperty ItemHeightProperty =
-            DependencyProperty.Register("ItemHeight", typeof(double), typeof(MyAdaptiveGridView), new PropertyMetadata(0.0));
-
-        public bool OneRowModeEnabled
-        {
-            get { return (bool)GetValue(OneRowModeEnabledProperty); }
-            set { SetValue(OneRowModeEnabledProperty, value); }
-        }
-        public static readonly DependencyProperty OneRowModeEnabledProperty =
-            DependencyProperty.Register("OneRowModeEnabled", typeof(bool), typeof(MyAdaptiveGridView), new PropertyMetadata(false));
-
-        public bool StretchContentForSingleRow
-        {
-            get { return (bool)GetValue(StretchContentForSingleRowProperty); }
-            set { SetValue(StretchContentForSingleRowProperty, value); }
-        }
-        public static readonly DependencyProperty StretchContentForSingleRowProperty =
-            DependencyProperty.Register("StretchContentForSingleRow", typeof(bool), typeof(MyAdaptiveGridView), new PropertyMetadata(true));
-
-        ScrollViewer scrollViewer;
         protected override void OnApplyTemplate()
         {
-            scrollViewer = GetTemplateChild("ScrollViewer") as ScrollViewer;
-            scrollViewer.ViewChanged += ScrollViewer_ViewChanged;
-            this.RegisterPropertyChangedCallback(DataLoadingProperty, new DependencyPropertyChangedCallback((obj, e) => {
-                if (!DataLoading)
-                {
-                    if (scrollViewer.ScrollableHeight == 0)
-                    {
-                        LoadMoreCommand?.Execute(null);
-                    }
-                }
-            }));
-
             base.OnApplyTemplate();
+
+            scrollViewer = GetTemplateChild("ScrollViewer") as ScrollViewer;
+            if (scrollViewer != null)
+            {
+                scrollViewer.ViewChanged += ScrollViewer_ViewChanged;
+            }
+
+            RegisterPropertyChangedCallback(DataLoadingProperty, (obj, e) =>
+            {
+                if (!DataLoading && scrollViewer != null && scrollViewer.ScrollableHeight == 0)
+                {
+                    LoadMoreCommand?.Execute(null);
+                }
+            });
         }
 
-        private void MyAdaptiveGridView_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        protected override void PrepareContainerForItemOverride(DependencyObject obj, object item)
         {
+            base.PrepareContainerForItemOverride(obj, item);
 
-            Trace.WriteLine("���ݱ��");
-            if (scrollViewer.ScrollableHeight == 0)
+            if (obj is FrameworkElement frameworkElement)
             {
-                LoadMoreCommand?.Execute(null);
+                frameworkElement.SetBinding(FrameworkElement.HeightProperty, new Binding
+                {
+                    Source = this,
+                    Path = new PropertyPath("ItemHeight"),
+                    Mode = BindingMode.TwoWay
+                });
+                frameworkElement.SetBinding(FrameworkElement.WidthProperty, new Binding
+                {
+                    Source = this,
+                    Path = new PropertyPath("ItemWidth"),
+                    Mode = BindingMode.TwoWay
+                });
+            }
+
+            if (obj is ContentControl contentControl)
+            {
+                contentControl.HorizontalContentAlignment = Microsoft.UI.Xaml.HorizontalAlignment.Stretch;
+                contentControl.VerticalContentAlignment = Microsoft.UI.Xaml.VerticalAlignment.Stretch;
+            }
+
+            if (_needContainerMarginForLayout)
+            {
+                _needContainerMarginForLayout = false;
+                RecalculateLayout(ActualWidth);
             }
         }
+
+        // ============ 尺寸计算 ============
+
+        private void OnGridSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.PreviousSize.Width != e.NewSize.Width)
+            {
+                RecalculateLayout(e.NewSize.Width);
+            }
+        }
+
+        private void ItemsOnVectorChanged(IObservableVector<object> sender, IVectorChangedEventArgs e)
+        {
+            if (!double.IsNaN(ActualWidth))
+            {
+                RecalculateLayout(ActualWidth);
+            }
+        }
+
+        private static void OnDesiredWidthChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var grid = d as MyAdaptiveGridView;
+            if (grid != null)
+            {
+                grid.RecalculateLayout(grid.ActualWidth);
+            }
+        }
+
+        private static void OnStretchContentForSingleRowChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var grid = d as MyAdaptiveGridView;
+            if (grid != null)
+            {
+                grid.RecalculateLayout(grid.ActualWidth);
+            }
+        }
+
+        private void RecalculateLayout(double containerWidth)
+        {
+            Panel itemsPanelRoot = ItemsPanelRoot;
+            double panelMargin = itemsPanelRoot != null ? itemsPanelRoot.Margin.Left + itemsPanelRoot.Margin.Right : 0.0;
+            double padding = Padding.Left + Padding.Right;
+            double border = BorderThickness.Left + BorderThickness.Right;
+
+            containerWidth = containerWidth - padding - panelMargin - border;
+            if (containerWidth > 0.0)
+            {
+                ItemWidth = Math.Floor(CalculateItemWidth(containerWidth));
+            }
+        }
+
+        private double CalculateItemWidth(double containerWidth)
+        {
+            if (double.IsNaN(DesiredWidth))
+            {
+                return DesiredWidth;
+            }
+
+            int columns = CalculateColumns(containerWidth, DesiredWidth);
+            if (Items != null && Items.Count > 0 && Items.Count < columns && StretchContentForSingleRow)
+            {
+                columns = Items.Count;
+            }
+
+            Thickness itemMargin = GetItemMargin(ItemContainerStyle);
+            if (itemMargin == default(Thickness))
+            {
+                _needContainerMarginForLayout = true;
+            }
+
+            return (containerWidth / columns) - itemMargin.Left - itemMargin.Right;
+        }
+
+        private static int CalculateColumns(double containerWidth, double itemWidth)
+        {
+            int columns = (int)Math.Round(containerWidth / itemWidth);
+            if (columns == 0)
+            {
+                columns = 1;
+            }
+            return columns;
+        }
+
+        private static Thickness GetItemMargin(Style style)
+        {
+            while (style != null)
+            {
+                foreach (var setter in style.Setters)
+                {
+                    if (setter is Setter s && s.Property == FrameworkElement.MarginProperty && s.Value is Thickness t)
+                    {
+                        return t;
+                    }
+                }
+                style = style.BasedOn;
+            }
+            return default(Thickness);
+        }
+
+        // ============ 滚动加载更多 ============
 
         private void ScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
         {
-            if (scrollViewer.VerticalOffset >= scrollViewer.ScrollableHeight - LoadMoreBottomOffset && CanLoadMore)
+            if (scrollViewer != null &&
+                scrollViewer.VerticalOffset >= scrollViewer.ScrollableHeight - LoadMoreBottomOffset &&
+                CanLoadMore)
             {
                 LoadMoreCommand?.Execute(null);
             }
-
         }
     }
 }
